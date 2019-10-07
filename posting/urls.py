e39d86397dfc5a2fc651854a26e010cd2b46e070
kenreitzdev <kenreitzdev@gmail.com>
2019-10-07 17:23:36 -0400

    Getting back into it

--- posting/urls.py
+++ posting/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.event_list, name="event_list"),
     path("post-<int:event_pk>/", views.post_info, name="post_info"),
-    path("view/", views.view_info, name="view_info"),
     path("monitor-<int:event_pk>/", views.event_monitor, name="event_monitor")
 ]
