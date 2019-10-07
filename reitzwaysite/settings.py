71423f6f6a3d7375a9299979b7aaf5bdfd538f94
kenreitzdev <kenreitzdev@gmail.com>
2019-10-07 18:12:14 -0400

    testing branch to remote

--- reitzwaysite/settings.py
+++ reitzwaysite/settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
+
+small change
+
 """
 
 import os
